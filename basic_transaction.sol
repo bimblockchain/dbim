f88c4d4acbac973ba3950a3da37a1b88dcffca2d
Arlav <dounas@gmail.com>
2020-03-22 12:55:20 +0000

    added withdraw to specific address

--- basic_transaction.sol
+++ basic_transaction.sol
@@ -12,9 +12,13 @@ contract tokens{
     function getBalance() public view returns(uint) {
         return address(this).balance;
     }
-    
+    //this withdraws money to the address that called the conract, through the msg.sender properties.
     function withdrawMoney() public{
         address payable to = msg.sender;
         to.transfer(this.getBalance());
+    }
+    //This allows us to specify which third adress to withdraw money to.
+    function withdrawMoneyTo(address payable _to) public {
+        _to.transfer(this.getBalance());
     }
 }
