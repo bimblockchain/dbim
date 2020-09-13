61d415828365c129a1cc613b2a925df88e41144e
Arlav <dounas@gmail.com>
2020-09-13 18:16:32 +0100

    Update startstop.sol
    updating comments and licence?

--- startstop.sol
+++ startstop.sol
@@ -1,18 +1,20 @@
 pragma solidity ^0.6.0;
 
+//SPDX-licence-identifier-mylicence.
+
 contract StartStopUpdate{
     address owner;
     bool public paused;
     
     //constructor is called during contract deployment only
     constructor() public {
-        owner = msg.sender;
+        owner = msg.sender; //owner of the contract is the sender
     
     
     }
     function setPaused(bool _paused) public {
         require(msg.sender ==owner, "you Are not the owner");
-        paused = _paused;
+        paused = _paused; //if the owner of the contrract triggers it then it works, if not then no.
         
     }
     
@@ -21,7 +23,7 @@ contract StartStopUpdate{
     }
     
     function withdrawAllMoney(address payable _to) public {
-        require(msg.sender == owner, "you are not the owner");
+        require(msg.sender == owner, "you are not the owner");  /if another account,other than the onwer,  tries to withdraw it will not work
         require(!paused, "contract is paused");
         _to.transfer(address(this).balance);
         
