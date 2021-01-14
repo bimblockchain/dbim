c8fbdf2c4c9ffb96d3fd10747c3aadf3ded8e2ec
arlav <dounas@gmail.com>
2021-01-14 19:51:41 +0000

    re-organisation
    folders and working towards templates

--- contracts/basic_transaction.sol
+++ contracts/basic_transaction.sol
@@ -12,12 +12,12 @@ contract tokens{
     function getBalance() public view returns(uint) {
         return address(this).balance;
     }
-    //this withdraws money to the address that called the conract, through the msg.sender properties.
+    //this withdraws money to the address that called the contract, through the msg.sender properties.
     function withdrawMoney() public{
         address payable to = msg.sender;
         to.transfer(this.getBalance());
     }
-    //This allows us to specify which third adress to withdraw money to.
+    //This allows us to specify which third address to withdraw money to.
     function withdrawMoneyTo(address payable _to) public {
         _to.transfer(this.getBalance());
     }
