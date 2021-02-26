c67c142a6ddee3da6a30eb944049870a318512be
arlav <dounas@gmail.com>
2021-02-26 12:35:38 +0000

    Update NFT_Topo_simple_obsolete.sol
    change the import of OpenZeppelin Contracts to use 3.1.0 release, as using the current versions broke remix as the current OpenZeppelin use 0.8.0 solidity compiler.

--- contracts/tokenisation/NFT_Topo_simple_obsolete.sol
+++ contracts/tokenisation/NFT_Topo_simple_obsolete.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.6.2;
 
-//05/-2-2020-re0factor the code for building components- to be used in a topology design process. 
+//05/-2-2020-re0factor the code for building components- to be used in a topology design process.
 //do so in a new contract.
 //for @Arlav on Sunday: Circuit Breaker pattern, comments per spec, tests!
 
@@ -13,7 +13,7 @@ pragma solidity ^0.6.2;
 ///
 
 
-/** @dev: simple impleementation of an ERC721 token marketplace, 
+/** @dev: simple impleementation of an ERC721 token marketplace,
  * with the addition of a circuit breaker desisgn and pullpayment & escrow accounts
  * to prevent reentrancy attacks. Pausable is used to pause all functions by the owner
  * as a circuit breaker pattern. Safe Math is used for uint256 to preveent overflows
@@ -21,10 +21,13 @@ pragma solidity ^0.6.2;
  * @notice: you can use this as a simple buildingToken marketplace for images or other digital buildingefacts
 */
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/payment/PullPayment.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Pausable.sol";
+//be careful wich veersion of OpenZeppelin contracts you arer using,
+//it is best ot use a specific previous version or the constant upgrades will break things
+//currently using release-v3.1.0 of the conracts which uses solidity 0.6.0 compiler.
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v3.1.0/contracts/token/ERC721/ERC721.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v3.1.0/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v3.1.0/contracts/payment/PullPayment.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v3.1.0/contracts/utils/Pausable.sol";
 
 
 contract buildingToken is ERC721, Ownable, PullPayment, Pausable {
@@ -39,7 +42,7 @@ using SafeMath for uint256;
   mapping (uint256 => buildingItem) private _buildingItems;
 
 
-///@dev: creating the buildingItem as a struct, toeknURI is the IPFS URI 
+///@dev: creating the buildingItem as a struct, toeknURI is the IPFS URI
   struct buildingItem {
     address seller;
     uint256 price;
@@ -49,10 +52,10 @@ using SafeMath for uint256;
 
 
   constructor() ERC721("buildingToken", "building") Pausable () public {
-      
+
   }
 
- 
+
   modifier buildingItemExist(uint256 id) {
     require(_buildingItems[id].exists, "Not Found");
     _;
@@ -78,7 +81,7 @@ using SafeMath for uint256;
 //@notice: executes a purchase of an item, after a check that the item exists.
 //@param: checks that the price is value offered is highr than the price.
 //@return: advances the item id, mints the actual ERC721 token connected with the buildingwork, assigns to the seller.
-//@return: sets the IPFS URI and transfers the 
+//@return: sets the IPFS URI and transfers the
 
   function purchasebuildingItem(uint256 buildingItemid) whenNotPaused external payable buildingItemExist(buildingItemid) {
     buildingItem storage buildingItem = _buildingItems[buildingItemid];
